d45bcc0295d633ad5b7c6ada4c3dc432a626740a
Tiago <frost.tiago@gmail.com>
2024-02-26 11:12:19 -0300

    Adição dos campos para tabela

--- MauiAppListaCompras/Models/Produto.cs
+++ MauiAppListaCompras/Models/Produto.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SQLite;
 
 namespace MauiAppListaCompras.Models
 {
-    internal class Produto
+    public class Produto
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao {  get; set; }
+        public double Quantidade {  get; set; }
+        public double Preco {  get; set; }
     }
 }
