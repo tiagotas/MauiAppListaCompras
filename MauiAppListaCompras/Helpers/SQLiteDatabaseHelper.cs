b83ef1886576f5f82242d3372e1b21a65f5c4661
Tiago <frost.tiago@gmail.com>
2024-02-26 11:38:42 -0300

    Começo do SQLite

--- MauiAppListaCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppListaCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiAppListaCompras.Models;
+using SQLite;
 
 namespace MauiAppListaCompras.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
-    }
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+    }'
 }
